29e9139a77f00816e86312971f7c1966db1d7020
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-12-19 20:24:46 +0530

    routing changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,22 @@ import { Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 import { EproductsComponent } from './eproducts/eproducts.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {
-        path:'',
-        component:AppComponent,
-    },
-
-    {
-        path:'products',
-        component:ProductsComponent
-    },
-    {
-        path:'eproducts',
-        component:EproductsComponent
-    },
+    
+    { 
+        path: '', 
+        component:HomeComponent,
+        children:[
+            {
+                path:'products',
+                component:ProductsComponent
+            },
+            {
+                path:'eproducts',
+                component:EproductsComponent
+            }
+        ]
+    }
 ];
