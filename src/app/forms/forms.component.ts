e88fcad85b6c4bd5759b11f02d6e2268bb792687
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-12-19 12:28:33 +0530

    forms

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms'
 @Component({
   selector: 'app-forms',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css'
 })
 export class FormsComponent {
+person:any=[]; 
 personForm:FormGroup
  constructor( private fb:FormBuilder){
   this.personForm=this.fb.group({
@@ -17,9 +18,24 @@ personForm:FormGroup
   });
  }
 
-   print(){
-       console.log(this.personForm.value)
+   add(){
+      this.person.push(this.personForm.value)
    }
+
+   del(index:any){
+
+   }
+   edit(){
+
+   }
+
+
+
+
+
+
+
+
 
   // fname=new FormControl()
   // lname=new FormControl()
