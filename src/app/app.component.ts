0bae598ef34b105400fcf649a34cad94d2007662
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-12-20 12:23:34 +0530

    parameterized routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CountriesComponent } from './countries/countries.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [CountriesComponent],
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'todolist';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { CountriesComponent } from './countries/countries.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomeComponent
+    },
+      {
+        path:'Countries',
+        component:CountriesComponent
+      },
+];
