8558583ec47ac7803aefea1aeb00627f12ba5860
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-12-27 20:36:46 +0530

    controler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Todo } from './todos/todos.model';
+import { selecttodo } from './todos/todos.selector';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todolist';
+  todos:any;
+  constructor( private  store:Store){
+    this.todos=this.store.select(selecttodo)
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideState, provideStore } from '@ngrx/store';
+import { todoreducer } from './todos/todos.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), provideClientHydration(withEventReplay()),
-    
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), provideClientHydration(withEventReplay()), 
+    provideStore(),
+  provideState({name:'todolist',reducer:todoreducer})]
 };
