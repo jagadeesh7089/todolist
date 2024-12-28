6ca828bfe662a1c6450668266aa1b397b3382e4f
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-12-28 12:09:58 +0530

    todo implements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,30 @@ import { RouterOutlet } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Todo } from './todos/todos.model';
 import { selecttodo } from './todos/todos.selector';
+import { AppState } from './todos/app.state';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms'
+import { addtodo } from './todos/todos.action';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule ,FormsModule,AsyncPipe ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todolist';
+  newtodo:string='';
   todos:any;
-  constructor( private  store:Store){
+  constructor( private  store:Store<AppState>){
     this.todos=this.store.select(selecttodo)
+      console.log('hiiiiii')
+  }
+
+
+  addtodo(){
+     const newtodo={title:this.newtodo,status:false}
+     this.store.dispatch(addtodo({todo:newtodo}))
+     
   }
 }
